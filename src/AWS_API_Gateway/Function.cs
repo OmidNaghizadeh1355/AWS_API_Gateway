5fb078a022307ae72106298768f7326aec49af8b
Omid Naghizadeh <omid.naghizadeh@icloud.com>
2022-04-10 21:47:00 +0200

    Add project files.

---
+++ src/AWS_API_Gateway/Function.cs
@@ -0,0 +1,33 @@
+using Amazon.Lambda.Core;
+using Amazon.Lambda.APIGatewayEvents;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace AWS_API_Gateway;
+
+public class Function
+{
+    
+    /// <summary>
+    /// A simple function that takes a string and does a ToUpper
+    /// </summary>
+    /// <param name="input"></param>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    public APIGatewayProxyResponse FunctionHandler(APIGatewayProxyRequest request, ILambdaContext context)
+    {
+        var name = "No Name";
+        if (request.QueryStringParameters != null && 
+            request.QueryStringParameters.ContainsKey("name"))
+        {
+            name = request.QueryStringParameters["name"];
+        }
+        context.Logger.Log($"Executing API Gateway! and receive {name}");
+        return new APIGatewayProxyResponse
+        {
+            StatusCode = 200,
+            Body = $"API Gateway received name value : {name}"
+        };
+    }
+}
